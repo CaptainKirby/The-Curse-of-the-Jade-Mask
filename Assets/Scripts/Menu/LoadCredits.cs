77bf26a06e22de86306ad4cffccd5e6c748d10ad
CaptainKirby <s.engelbrecht@gmail.com>
2015-06-24 23:10:52 +0200

    Credits

---
+++ Assets/Scripts/Menu/LoadCredits.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCredits
+: MonoBehaviour {
+
+	public GameObject creditsObject;
+	void Start()
+	{
+		creditsObject = GameObject.Find ("Credits");
+//		optionsObject.SetActive (false);
+	}
+	public void OpenCredits()
+	{
+//		Debug.Log ("NGU#");
+//		optionsObject.SetActive (true);
+		creditsObject.GetComponent<Canvas> ().enabled = true;
+	}
+	public void CloseCredits()
+	{
+		creditsObject.GetComponent<Canvas> ().enabled = false;
+
+	}
+
+//	public void OpenOptionsScene()
+//	{
+//		Application.LoadLevel ("Options");
+//	}
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
